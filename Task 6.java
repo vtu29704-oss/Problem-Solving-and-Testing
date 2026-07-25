0361948fd65b91786f49ec1743ad984561bfb289
vtu29704-oss <vtu29704@veltech.edu.in>
2026-07-25 10:12:12 +0530

    Add files via upload

---
+++ Task 6.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        System.out.print("Enter option (1-Even, 2-Odd): ");
+        int opt = sc.nextInt();
+
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+
+            if (opt == 1 && digit % 2 == 0) {
+                sum += digit;
+            } else if (opt == 2 && digit % 2 != 0) {
+                sum += digit;
+            }
+
+            num = num / 10;
+        }
+
+        System.out.println("Sum = " + sum);
+
+        sc.close();
+    }
+}
