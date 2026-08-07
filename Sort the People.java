31326e239db322839be11264d73eabbcfac0da1b
vtu29704-oss <vtu29704@veltech.edu.in>
2026-08-07 19:31:59 +0530

    Add files via upload

---
+++ Sort the People.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+
+        HashMap<Integer, String> map = new HashMap<>();
+
+        for (int i = 0; i < names.length; i++) {
+            map.put(heights[i], names[i]);
+        }
+
+        Arrays.sort(heights);
+
+        String[] result = new String[names.length];
+        int index = 0;
+
+        for (int i = heights.length - 1; i >= 0; i--) {
+            result[index++] = map.get(heights[i]);
+        }
+
+        return result;
+    }
+}
